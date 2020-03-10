2c9ce5bc01c5528e84b29a4cffe00243a558108e
Zsolt Toth <tothzs87@gmail.com>
2020-03-10 11:22:06 +0100

    promise example
    callbacks are added to the gross price calculation function.
    Darck Chocholate price was fixed.

--- 2_js_review/promise/GrossPriceCalculator.js
+++ 2_js_review/promise/GrossPriceCalculator.js
@@ -16,7 +16,20 @@ class GrossPriceCalculator{
     }
 
     totalGrossPrice_cb(products, callback, error){
-
+        var wasError = false;
+        var totalGrossPrice = products.reduce((total, cur)=>{
+            if(cur.price <= 0.0){
+                error(`The price of ${cur.name} is negative (${cur.price})!`);
+                wasError = true;
+            }
+            if(cur.isTaxable){
+                return total + cur.price * (100.0 + this.vatRate) / 100.0
+            }
+            return total + cur.price;
+        },0);
+        if(!wasError) {
+            callback(totalGrossPrice);
+        }
     }
 
 }
--- 2_js_review/promise/main.js
+++ 2_js_review/promise/main.js
@@ -2,7 +2,7 @@ const products = [
     { name : "Apple", price : 5, isTaxable : true},
     { name : "Banana", price : 12, isTaxable : true},
     { name : "Cherry", price : 8, isTaxable : false},
-    { name : "Dark Chocolate", price : -23, isTaxable : true},
+    { name : "Dark Chocolate", price : 23, isTaxable : true},
 ];
 
 const priceCalculator = require('./GrossPriceCalculator');
