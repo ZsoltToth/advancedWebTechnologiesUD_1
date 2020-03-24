eee3793a2a6e875459db6a7f77b8abe4bc991163
Zsolt Toth <tothzs87@gmail.com>
2020-03-24 10:11:50 +0100

    Authors are generated

--- 3_faker/bookFaker.js
+++ 3_faker/bookFaker.js
@@ -1,1 +1,17 @@
 const faker = require('faker');
+
+generateAurhor = () => {
+    return {
+        name : faker.name.firstName()+' ' + faker.name.lastName(),
+        country : faker.address.country()
+    };
+};
+
+var authors = [];
+for(let i = 0; i < 100; i++){
+    author = generateAurhor();
+    author.id=i;
+    authors.push(author);
+}
+
+console.log(authors);
