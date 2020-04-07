d0e40158009abe9d3e6609a71e0a6efccc63a596
Zsolt Toth <tothzs87@gmail.com>
2020-04-07 10:26:17 +0200

    React State Example
    handleNameChange function was added.

--- 5_react_state/src/components/ToDoList.js
+++ 5_react_state/src/components/ToDoList.js
@@ -2,6 +2,26 @@ import React from 'react'
 
 class  ToDoList extends React.Component{
 
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            form : {
+                name : 'Change Tires',
+                description : 'There are 4 tires.',
+                estimation : 2.5
+            },
+            todos : []
+        };
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+    handleNameChange(event){
+        let st = this.state;
+        st.form.name = event.target.value;
+        this.setState(st);
+    }
+
     render() {
         return (
             <div>
@@ -11,15 +31,31 @@ class  ToDoList extends React.Component{
                         <tbody>
                         <tr>
                             <td>Name</td>
-                            <td><input type="text"/></td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value={this.state.form.name}
+                                    onChange={this.handleNameChange}
+                                />
+                            </td>
                         </tr>
                         <tr>
                             <td>Description</td>
-                            <td><input type="text"/></td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value={this.state.form.description}
+                                />
+                            </td>
                         </tr>
                         <tr>
                             <td>Estimation</td>
-                            <td><input type="number"/></td>
+                            <td>
+                                <input
+                                    type="number"
+                                    value={this.state.form.estimation}
+                                />
+                            </td>
                         </tr>
                         <tr>
                             <td colSpan={2}>
