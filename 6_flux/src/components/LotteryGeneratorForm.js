e6e51f2e0d70a4467c9ba7f963cc393cbdf9f767
Zsolt Toth <tothzs87@gmail.com>
2020-04-14 10:15:44 +0200

    LotteryGeneratorForm.js has been added

---
+++ 6_flux/src/components/LotteryGeneratorForm.js
@@ -0,0 +1,75 @@
+import React from 'react'
+
+class LotteryGeneratorForm extends React.Component{
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            min : 1,
+            max : 90,
+            pickCnt : 5
+        }
+    }
+
+    render(){
+        return (
+            <table>
+                <tbody>
+                <tr>
+                    <td>Min</td>
+                    <td>
+                        <input
+                            type={"number"}
+                            value={this.state.min}
+                            onChange={(e)=>{
+                                let st = this.state;
+                                st.min = Math.round(e.target.value);
+                                this.setState(st);
+                            }}
+                        />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Max</td>
+                    <td>
+                        <input
+                            type={"number"}
+                            value={this.state.max}
+                            onChange={(e)=>{
+                                let st = this.state;
+                                st.max = Math.round(e.target.value);
+                                this.setState(st);
+                            }}
+                        />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Pick Count</td>
+                    <td>
+                        <input
+                            type={"number"}
+                            value={this.state.pickCnt}
+                            onChange={(e)=>{
+                                let st = this.state;
+                                st.pickCnt = Math.round(e.target.value);
+                                this.setState(st);
+                            }}
+                        />
+                    </td>
+                </tr>
+                <tr>
+                    <td colSpan={2}>
+                        <button onClick={()=>{
+                            console.log(this.state);
+                        }}
+                        >Generate</button>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        );
+    }
+}
+
+export default LotteryGeneratorForm;
