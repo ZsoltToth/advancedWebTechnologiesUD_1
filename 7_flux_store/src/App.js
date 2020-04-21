2a990bd293d48cd3a5e25077a65c0efdaa013823
Zsolt Toth <tothzs87@gmail.com>
2020-04-21 10:20:08 +0200

    Form is added.

--- 7_flux_store/src/App.js
+++ 7_flux_store/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.scss';
+import ShoppingCartForm from "./components/ShoppingCartForm";
 
 function App() {
   return (
     <div className="container-fluid">
         <div className="row">
-            <div className="col-md-12">Shopping Cart Form</div>
+            <div className="col-md-12"><ShoppingCartForm/></div>
         </div>
         <div className="row">
           <div className="col-md-3">Brief List</div>
---
+++ 7_flux_store/src/components/ShoppingCartForm.js
@@ -0,0 +1,71 @@
+import React from 'react'
+
+class ShoppingCartForm extends React.Component{
+
+
+    constructor() {
+        super();
+        this.state = {
+            name : '',
+            description : '',
+            price : 0
+        };
+    }
+
+    render(){
+        return (
+            <table className="table">
+                <tbody>
+                <tr>
+                    <td>Name</td>
+                    <td><input
+                        type="text"
+                        value={this.state.name}
+                        onChange={(e) => {
+                            this.setState({
+                                name: e.target.value
+                            });
+                        }
+                        }/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Description</td>
+                    <td><input
+                        type="text"
+                        value={this.state.description}
+                        onChange={(e) => {
+                            this.setState({
+                                description: e.target.value
+                            });
+                        }
+                        }/>
+                    </td>
+                </tr>
+                    <tr>
+                        <td>Price</td>
+                        <td><input
+                            type="number"
+                            value={this.state.price}
+                            onChange={(e) => {
+                                this.setState({
+                                    price: e.target.value
+                                });
+                            }
+                            }/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2}>
+                            <button
+                                className="btn btn-info"
+                                onClick={()=>{console.log(this.state)}}
+                            >Add</button></td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
+}
+
+export default ShoppingCartForm;
